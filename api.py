0e6fc52389ad5da2de9926f3f921d486fe9c8758
0xVict0r <74269639+0xVict0r@users.noreply.github.com>
2022-11-30 18:14:44 +0100

    added comments

--- api.py
+++ api.py
@@ -3,12 +3,12 @@ from functions import *
 
 app = FastAPI()
 
-
+# Home endpoint
 @app.get('/')
 def home():
-    return {"Data": "Home"}
+    return {"Data": "Use /get_daily_route_capacity/origin_icao&destination_icao"}
 
-
+# Main route endpoint (run using "uvicorn api:app --reload")
 @app.get('/get_daily_route_capacity/{origin_icao}&{destination_icao}')
 def daily_cargo(origin_icao: str, destination_icao: str):
     return route_daily_capacity(origin_icao, destination_icao)
--- functions.py
+++ functions.py
@@ -3,8 +3,11 @@ import numpy as np
 
 
 def gather_flight_data():
+
+    # Read the first csv file and make a pd dataframe
     flight_df = pd.read_csv(r"./data/flight_events/2022-10-03.csv", sep=";")
 
+    # Loop through the other ones and append them to the initial one
     for i in np.arange(4, 10):
         temp = pd.read_csv(f"./data/flight_events/2022-10-0{i}.csv", sep=";")
         flight_df = pd.concat([flight_df, temp], ignore_index=True)
@@ -13,11 +16,15 @@ def gather_flight_data():
 
 
 def gather_aircraft_data():
+
+    # Read the json file and make a pd dataframe
     aircraft_df = pd.read_json(r'./data/airplane_details.json', lines=True)
 
+    # Calculate the average cargo density to populate the missing values
     average_cargo_density = aircraft_df["payload"].mean(
         skipna=True) / aircraft_df["volume"].mean(skipna=True)
 
+    # Loop through the df and add volume where it is missing
     for i in range(len(aircraft_df)):
         if pd.isna(aircraft_df.at[i, "volume"]):
             aircraft_df.at[i, "volume"] = aircraft_df.at[i,
@@ -27,12 +34,16 @@ def gather_aircraft_data():
 
 
 def make_capacity_table(flight_df, aircraft_df, save_to_csv):
+
+    # Create the cargo capacity df from the existing flight df
     cargo_capacity_df = flight_df[["callsign", "destination_icao", "equipment", "flight", "flight_id",
                                    "operator", "origin_icao", "registration"]].drop_duplicates(subset=["flight_id"])
 
+    # Remove all the flights where the aircraft is not in aircraft_df (a majority of helicopters)
     cargo_capacity_df = cargo_capacity_df[cargo_capacity_df["equipment"].isin(
         aircraft_df["code_icao"])].reset_index(drop=True)
 
+    # Loop through the df to add capacity weight and volume to each flight, by checking the aircraft type
     for i in range(len(cargo_capacity_df)):
         aircraft_icao = cargo_capacity_df.at[i, "equipment"]
         cargo_capacity_df.at[i, "capacity_weight"] = aircraft_df[aircraft_df["code_icao"]
@@ -40,6 +51,7 @@ def make_capacity_table(flight_df, aircraft_df, save_to_csv):
         cargo_capacity_df.at[i, "capacity_volume"] = aircraft_df[aircraft_df["code_icao"]
                                                                  == aircraft_icao]["volume"].values[0]
 
+    # If true, save the data to a new csv, useful to save the data and use it instead of running this everytime
     if save_to_csv:
         cargo_capacity_df.to_csv(r"./data/cargo_capacity.csv")
 
@@ -48,15 +60,18 @@ def make_capacity_table(flight_df, aircraft_df, save_to_csv):
 
 def route_daily_capacity(origin_icao, destination_icao):
 
+    # Read the capacity df (the one saved using the funtion above)
     capacity_df = pd.read_csv(r"./data/cargo_capacity.csv", index_col=0)
 
+    # Filter the df by the origin and destination airports
     route_df = capacity_df.loc[(capacity_df["origin_icao"] == origin_icao) &
                                (capacity_df["destination_icao"] == destination_icao)]
 
+    # Calculate the average daily weight and volume on that route (/7 because the initial data is an aggregate of 7 days of flights)
     route_daily_weight = route_df["capacity_weight"].sum() / 7
     route_daily_volume = route_df["capacity_volume"].sum() / 7
 
-    return {"Origin Airport": origin_icao, "Destination Airport": destination_icao, "Daily Cargo Weight [kg]": route_daily_weight, "Daily Cargo Volume [m3]": route_daily_volume}
+    return {"Origin Airport": origin_icao, "Destination Airport": destination_icao, "Daily Capacity Weight [kg]": route_daily_weight, "Daily Capacity Volume [m3]": route_daily_volume}
 
 
 if __name__ == "main":
